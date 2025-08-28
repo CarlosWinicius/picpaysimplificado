177028d21ee2b4874011f704fbea8e00a5e33d44
Carlos Winicius <carlos.winicius055@gmail.com>
2025-08-27 22:24:26 -0300

    feat: create  User Repository

---
+++ src/main/java/com/picpaysimplificado/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package com.picpaysimplificado.repositories;
+
+import com.picpaysimplificado.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public class interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByDocument(String document);
+
+    Optional<User> findUserById(Long id);
+
+
+}
