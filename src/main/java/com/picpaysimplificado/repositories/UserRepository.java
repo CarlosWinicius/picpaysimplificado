602ae15c99f8e10f289a7f2757daaac487c56023
Carlos Winicius <carlos.winicius055@gmail.com>
2025-08-27 22:27:41 -0300

    feat: create  TransactionRepository

--- src/main/java/com/picpaysimplificado/repositories/UserRepository.java
+++ src/main/java/com/picpaysimplificado/repositories/UserRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public class interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findUserByDocument(String document);
 
